1ed776277523ed9533d237c166a8e2a1efb8b07e
Marcos <130710734+MarcosRGZ38@users.noreply.github.com>
2024-09-13 17:21:56 +0200

    mm

--- PythonEquivalents/secrets.py
+++ PythonEquivalents/secrets.py
@@ -1,8 +1,8 @@
 currentKeyLenghtBytes:int = 16
-currentDynamicKey:str = "Llaves de UBLOX para THINGSTREAM"
+currentDynamicKey:str = "319ad6e4e7deded079631020eb1dfe21"
 currentKeyGPSWeek:int = 2254
 currentKeyGPSToW: int = 0
-nextDynamicKey:str = "LLaves de ublox para THINGSTREAM"
+nextDynamicKey:str = "319ad6e4e7deded079631020eb1dfe21"
 nextKeyGPSWeek = 2258
 nextKeyGPSToW = 0
 
